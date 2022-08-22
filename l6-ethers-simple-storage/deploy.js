1d2b982dd8febe1a08e4000770d5db2c61e7c0d2
mgorem <oremgitonga22@gmail.com>
2022-08-22 19:03:02 +0300

    deploy contract locally,ganache,promises,overrrides(gasPrice)

--- l6-ethers-simple-storage/deploy.js
+++ l6-ethers-simple-storage/deploy.js
@@ -1,5 +1,25 @@
 // For deploying SimpleStorage.sol
-async function main() {}
+const ethers = require("ethers");
+const fs = require("fs-extra");
+
+async function main() {
+  const provider = new ethers.providers.JsonRpcProvider(
+    "http://127.0.0.1:7545"
+  );
+  const wallet = new ethers.Wallet(
+    "7a7a8877c5e9b309da07846d0500b66555d9bd329a789c0a0f95738f0e23de97",
+    provider
+  );
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf8"
+  );
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("Deploying, please wait...");
+  const contract = await contractFactory.deploy();
+  console.log(contract);
+}
 
 main()
   .then(() => process.exit(0))
